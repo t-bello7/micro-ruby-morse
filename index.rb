d1982ffecdeb1b68fdd200e857ca7cd8f4b7b3d4
brainconnect93 <brainconnect0@gmail.com>
2022-09-01 09:57:18 +0100

    Start the decoding the morse code characters

--- index.rb
+++ index.rb
@@ -1,4 +1,12 @@
 #create morse hash
+# decoding the characters
+def decode_char(char)
+  morse_char = {
+    ".-" => "A",
+    "-..." => "B",
+    "-.-." => "C",
+    "-.." => "D",
+  }
 
 # create decode method that take in one argument of string
 # Split the string into words by checking where 3 spaces are 
